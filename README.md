789038874c28090048f3a8eea92e16df0341f56b
saimakhwaja <42872857+saimakhwaja@users.noreply.github.com>
2023-01-31 12:46:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-k1ng81
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-k1ng81)
---
+++ index.js
@@ -0,0 +1,13 @@
+////document.getElementById("count-el").innerText = 5
+
+//let count = 0;
+
+//console.log(count)
+
+//let myAge = 43;
+
+//console.log(myAge)
+
+function increment() {
+  console.log('The button was clicked');
+}
